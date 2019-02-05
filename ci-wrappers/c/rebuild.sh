7ba3a836b861d3532a98fa05364bd1e594e62480
Bert Kleewein <bertk@microsoft.com>
2019-02-04 19:45:44 -0800

    build with raw trace

--- ci-wrappers/c/rebuild.sh
+++ ci-wrappers/c/rebuild.sh
@@ -5,7 +5,7 @@ mkdir /wrapper/build
 cd /wrapper/build 
 [ $? -eq 0 ] || { echo "cd build failed "; exit 1; }
 
-cmake -D BUILD_TESTING=OFF -D use_edge_modules=ON  -D skip_samples=ON C_SDK_ROOT=/sdk -DCMAKE_BUILD_TYPE=Debug -D use_amqp=ON -D use_mqtt=ON -D use_http=ON ..
+cmake -D BUILD_TESTING=OFF -D use_edge_modules=ON  -D skip_samples=ON C_SDK_ROOT=/sdk -DCMAKE_BUILD_TYPE=Debug -D use_amqp=ON -D use_mqtt=ON -D use_http=ON -DcompileOption_C:STRING=-DENABLE_RAW_TRACE  ..
 [ $? -eq 0 ] || { echo "cmake failed"; exit 1; }
 
 make edge_e2e_rest_server
